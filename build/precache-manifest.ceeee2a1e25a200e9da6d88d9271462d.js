23b8d31c32c78fd9e74c15bef619df4026b051af
redlogicapps <blejdea.ovidiu@gmail.com>
2020-05-04 14:25:24 +0200

    adding files for github pages

---
+++ build/precache-manifest.ceeee2a1e25a200e9da6d88d9271462d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e458155d2acc7f59b554b9d7a2f26d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c1301c958dca89d3c13c",
+    "url": "/monsters-rolodex/static/css/main.4db9a7af.chunk.css"
+  },
+  {
+    "revision": "37ff977f4d55dc3949b1",
+    "url": "/monsters-rolodex/static/js/2.b9882d54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b9882d54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1301c958dca89d3c13c",
+    "url": "/monsters-rolodex/static/js/main.9ef15f87.chunk.js"
+  },
+  {
+    "revision": "3acb30cb3387f9458b0a",
+    "url": "/monsters-rolodex/static/js/runtime-main.11c5fdd8.js"
+  }
+]);
